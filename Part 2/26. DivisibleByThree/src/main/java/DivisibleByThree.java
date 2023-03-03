923c069c566010c5e84eb1eb4a50793fb1af02ba
murdoch24 <murdoch24@gmail.com>
2023-03-03 11:56:51 +0000

    Add files via upload

---
+++ Part 2/26. DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class DivisibleByThree {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        divisibleByThreeInRange(3, 5);
+
+    }
+    
+    public static void divisibleByThreeInRange(int beginning, int end){
+        for (int i= beginning; i < end + 1; i++){
+            if (i % 3 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+
+    
+
+}
